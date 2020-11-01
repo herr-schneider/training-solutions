117140e13bbbaea1135c6b2d07ccb9185b4fd611
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-11-01 13:49:12 +0100

    First commit

--- src/main/java/classstructureattributes/Client.java
+++ src/main/java/classstructureattributes/Client.java
@@ -1,7 +1,32 @@
 package classstructureattributes;
 
 public class Client {
+
+
     String name;
     int yearOfBirth;
     String address;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getYear() {
+        return yearOfBirth;
+    }
+
+    public String getAddress() {
+        return name;
+    }
+
+    public int getAge (int thisyear) {
+        return  thisyear - yearOfBirth;
+    }
+
+    public void changeName (String name) {
+       this.name = name;
+    }
+
+
+
 }
--- src/main/java/classstructureattributes/ClientMain.java
+++ src/main/java/classstructureattributes/ClientMain.java
@@ -16,8 +16,15 @@ public class ClientMain {
         String message3 = "Where are you live?";
         System.out.println(message3);
         client.address = scanner.nextLine();
-        System.out.println("Name: " + client.name);
-        System.out.println("Year: " + client.yearOfBirth);
-        System.out.println("Address: " + client.address);
+        String message4 = "Year of your birth, once more?";
+        System.out.println(message4);
+        System.out.println("Name: " + client.getName() );
+        System.out.println("Modify tha name:");
+        String name2 = scanner.nextLine();
+        client.changeName(name2);
+        System.out.println("Name: " + client.getName() );
+        System.out.println("Year: " + client.getYear() );
+        System.out.println("Age: " + client.getAge (2020));
+        System.out.println("Address: " + client.getAddress() );
     }
 }
