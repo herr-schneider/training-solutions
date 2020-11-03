39247bbb84f644e94472dbf940d058f2054350e4
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-11-03 15:56:33 +0100

    Complete

--- src/main/java/week2/PhoneMain.java
+++ src/main/java/week2/PhoneMain.java
@@ -1,26 +1,26 @@
 package week2;
 
-import localvariables.Distance;
-
 import java.util.Scanner;
 
 public class PhoneMain {
     public static void main(String[] args) {
 
-        System.out.println("Tipus: ");
-        Scanner scanner = new Scanner(System.in);
+       System.out.println("Tipus: ");
+       Scanner scanner = new Scanner(System.in);
        String type = scanner.nextLine();
-        System.out.println("Mem meret: ");
+       System.out.println("Mem meret: ");
        int mem = scanner.nextInt();
+       scanner.nextLine();
        Phone phone = new Phone(type, mem);
 
-        System.out.println("Tipus2: ");
-        type = scanner.nextLine();
-        System.out.println("Mem meret2: ");
-        mem = scanner.nextInt();
-        Phone phoneother = new Phone(type, mem);
+       System.out.println("Tipus2: ");
+       type = scanner.nextLine();
+       System.out.println("Mem meret2: ");
+       mem = scanner.nextInt();
+       scanner.nextLine();
+       Phone phoneother = new Phone(type, mem);
 
-        System.out.println("Telefon tipusa:" + phone.getType() + "Mem merete:" + phone.getMem());
-        System.out.println("Telefon tipusa2:" + phoneother.getType() + "Mem merete:" + phoneother.getMem());
+       System.out.println("Telefon tipusa:" + phone.getType() + "Mem merete:" + phone.getMem());
+       System.out.println("Telefon tipusa2:" + phoneother.getType() + "Mem merete:" + phoneother.getMem());
 
     }}
