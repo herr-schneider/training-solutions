4581d577a65fd0fb520f8bdf1edf6208341d52d1
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-11-07 18:43:10 +0100

    not working

--- src/main/java/week2/Controller.java
+++ src/main/java/week2/Controller.java
@@ -9,24 +9,73 @@ public class Controller {
     public void readOffice() {}
 
     public List<String> printMenu() {
-        String[] menuPoints = {"1. Meeting room in order.", "2. Meeting room in reverse order.", "3. Every event meeting rooms", "4. Areas", "5. Search", "6. Particularly search. ", "Area search."};
+        String[] menuPoints = {"1. Meeting room in order.", "2. Meeting room in reverse order.", "3. Every event meeting rooms", "4. Areas", "5. Search", "6. Particularly search. ", "7. Area search."};
         List<String> menu = Arrays.asList(menuPoints);
         return menu;
         }
 
-    public List<String> runMenu(Office office, int chosen) {
-        switch(chosen) {
-            case 1:{
-            return office.printNames();
+
+
+
+    public static void main(String[] args) {
+        Office office = new Office();
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("How many room are there:");
+        int numberOfItem = scanner.nextInt();
+        scanner.nextLine();
+        for (int i = 0; i < numberOfItem; i++) {
+            System.out.println("Name of " + (i + 1) + ". meeting room:");
+            String name = scanner.nextLine();
+            System.out.println("Width of meeting room:");
+            int width = scanner.nextInt();
+            scanner.nextLine();
+            System.out.println("Length of meeting room:");
+            int length = scanner.nextInt();
+            scanner.nextLine();
+            MeetingRoom meetingRoom = new MeetingRoom(name, width, length);
+            office.addMeetingRoom(meetingRoom);
+        }
+
+        Controller controlMenu = new Controller();
+
+        for (String points : controlMenu.printMenu()) {
+            System.out.println(points);
+        }
+
+
+        System.out.println("What do you want?");
+        String chosen = scanner.nextLine();
+        if (chosen.equals("1")) {
+            for (String points : office.printNames()) {
+                System.out.println(points);
             }
-            case 2:{
-            return office.printNamesReverse();
+        } else {
+            if (chosen.equals("2")) {
+                for (String points : office.printNamesReverse()) {
+                    System.out.println(points);
+                }
+            } else {
+                if (chosen.equals("3")) {
+                    for (String points : office.printEventNames()) {
+                        System.out.println(points);
+                    }
+                } else {
+                    if (chosen.equals("4")) {
+                        for (String points : office.printAreas()) {
+                            System.out.println(points);
+                        }}
+                }
             }
-            case 3:{
-            return office.printEventNames();
+            /* case "3": {
+                for (String points : office.printEventNames()) {
+                    System.out.println(points);
+                }
             }
+                default: {
+                    System.out.println("Hülye vagy");}
             case 4:{
-            return office.printAreas();
+                return office.printAreas();
             }
             case 5:{
                 Scanner scanner = new Scanner(System.in);
@@ -47,50 +96,16 @@ public class Controller {
                 scanner.nextLine();
                 return office.printAreasLargerThan(minimumArea);
             }
+
             default:{
                 String[] output = {"Adam", "Eve", "Jonathan"};
                 List<String> outList = Arrays.asList(output);
                 return outList;
-            }
-            }
+            } */
+
         }
+    }}
 
 
-    public static void main(String[] args) {
-        Office office = new Office();
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("How many room are there:");
-        int numberOfItem = scanner.nextInt();
-        scanner.nextLine();
-        for (int i=0; i < numberOfItem; i++) {
-            System.out.println("Name of " + (i+1) + ". meeting room:");
-            String name = scanner.nextLine();
-            System.out.println("Width of meeting room:");
-            int width = scanner.nextInt();
-            scanner.nextLine();
-            System.out.println("Length of meeting room:");
-            int length = scanner.nextInt();
-            scanner.nextLine();
-            MeetingRoom meetingRoom = new MeetingRoom(name, width, length);
-            office.addMeetingRoom(meetingRoom);
-        }
-
-        Controller controlMenu = new Controller();
-
-        for (String points : controlMenu.printMenu()) {
-            System.out.println(points);
-        }
-
-
-        System.out.println("What do you want?");
-        int chosen = scanner.nextInt();
-        scanner.nextLine();
-        Controller controller = new Controller();
-        for (String outText : controller.runMenu(office, chosen)) {
-            System.out.println(outText);
-        }
-
-    }
-}
 
