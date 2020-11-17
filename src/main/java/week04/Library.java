ecb2207c34a939c7954fb5b43559f2785e153ca7
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-11-17 21:08:30 +0100

    This is what I missed

---
+++ src/main/java/week04/Library.java
@@ -0,0 +1,41 @@
+package week04;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Library {
+    private List<Book> books = new ArrayList<>();
+
+    public void addBooks(Book book) {
+        books.add(book);
+    }
+    public void loadFromAnyFile(String sourceFile) {
+        try (Scanner scanner = new Scanner(Library.class.getResourceAsStream(sourceFile))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] names = line.split(";");
+                int year = Integer.parseInt(names[3]);
+                Book newBook = new Book(names[0], names[1], names[2], year);
+                addBooks(newBook);
+            }
+        }
+
+    }
+    public void loadFromFile() {
+        try (Scanner scanner = new Scanner(Library.class.getResourceAsStream("/books.csv"))) {
+            while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                String[] names = line.split(";");
+                int year = Integer.parseInt(names[3]);
+                Book newBook = new Book(names[0], names[1], names[2], year);
+                addBooks(newBook);
+            }
+        }
+
+    }
+
+    public List<Book> getBooks () {
+        return this.books;
+    }
+}
