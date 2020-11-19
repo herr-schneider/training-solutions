900feff2a0699f4579b64c573216d8d598a757ba
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-11-19 21:14:28 +0100

    Only commit

--- src/main/java/schoolrecords/ClassRecords.java
+++ src/main/java/schoolrecords/ClassRecords.java
@@ -1,14 +1,57 @@
 package schoolrecords;
 
 import java.util.List;
+import java.util.Random;
 
 public class ClassRecords {
     String nameOfClass;
+    Random random;
     List<Student> studentsOfClass;
 
-    ublic boolean addStudent(Student student) // felvesz egy diákot az osztályba
-    public boolean removeStudent(Student student) //kivesz egy diákot az osztályból
+    public ClassRecords(String nameOfClass, Random random) {
+        this.nameOfClass = nameOfClass;
+        this.random = random;
+    }
+
+    private boolean isEmpty(String str) {
+        if (str == null || str.isBlank() || str.isEmpty()) {
+            return true;
+        }
+        return false;
+    }
+
+    public boolean addStudent(Student student) // felvesz egy diákot az osztályba
+    {
+        if (student == null) {
+            throw new IllegalArgumentException("Empty string!");
+        }
+
+        for (Student studentAlready : studentsOfClass) {
+            if (studentAlready == student) {
+                return true;
+            }
+            studentsOfClass.add(student);
+            return false;
+        }
+    }
+
+
+    public boolean removeStudent(Student remStudent) {
+        if (remStudent == null) {
+                throw new IllegalArgumentException("Empty string!");
+            }
+
+            for (Student studentAlready : studentsOfClass) {
+                if (studentAlready != remStudent) {
+                    return true;
+                }
+            }
+            studentsOfClass.remove(studentsOfClass.indexOf(remStudent));
+            return false;
+        }
+
     public double calculateClassAverage() //osztályátlagot számol, minden diákot figyelembe véve
+
     public double calculateClassAverageBySubject(Subject subject) //tantárgy szerinti osztályátlagot számol,
     // kihagyja azon diákokat, akiknek az adott tantárgyból nincs jegye
     public Student findStudentByName(String name) // név szerint megkeres egy diákot az osztályban
