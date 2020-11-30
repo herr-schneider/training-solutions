73f2247e265da23a7110fdb29ec77c9485d0c8e6
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-11-30 14:03:11 +0100

    Nagyon jó

---
+++ src/main/java/week06d01/ListSelector.java
@@ -0,0 +1,39 @@
+package week06d01;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSelector {
+List<String> list = new ArrayList<>();
+
+    /*public ListSelector(List<String> list) {
+        this.list = list;
+    }*/
+
+    public void addToList(String str) {
+        list.add(str);
+    }
+
+    public String selector() {
+    StringBuilder result = new StringBuilder("[");
+    for (int i = 0; i < list.size(); i++) {
+        if (i > 0 && i%2==0) {
+            result.append(list.get(i)+ " ");
+        }
+    }
+
+    result.replace(result.length()-1,result.length(),"]");
+    return result.toString();
+    }
+
+    public void create(String... elementsOfList) {
+        if (elementsOfList == null) { // || elementsOfList.length <1
+            throw new IllegalArgumentException("There is no element!");
+        }
+        for (int i =0 ; i < elementsOfList.length ; i++){
+            list.add(elementsOfList[i]);
+        }
+    }
+
+}
---
+++ src/test/java/week06d01/ListSelectorTest.java
@@ -0,0 +1,34 @@
+package week06d01;
+
+import org.junit.Before;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ListSelectorTest {
+    ListSelector test = new ListSelector();
+
+
+    @Test
+    public void testList() {
+        test.addToList("zero");
+        test.addToList("one");
+        test.addToList("two");
+        test.addToList("three");
+        test.addToList( "four");
+        assertEquals("[two four]", test.selector());
+    }
+
+    @Test
+    public void nullList() throws IllegalArgumentException {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> test.create(null));
+        assertEquals("There is no element!", ex.getMessage());
+    }
+
+    @Test
+    public void testListVararg() {
+        test.create("zero", "one", "two", "three", "four");
+        assertEquals("[two four]", test.selector());
+    }
+}
