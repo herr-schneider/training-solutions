cf61b2b991683d667248ced6bdc9a416a9d2758f
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-12-07 16:12:51 +0100

    4th

--- src/main/java/week07d01/MathAlgorithms.java
+++ src/main/java/week07d01/MathAlgorithms.java
@@ -11,7 +11,7 @@ public class MathAlgorithms {
     private static List<Integer> primNumbers;
 
     static {
-        Integer[] prims = new Integer[]{2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271};
+        Integer[] prims = new Integer[]{1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109, 113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179, 181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241, 251, 257, 263, 269, 271};
         primNumbers = Arrays.asList(prims);
     }
 
@@ -32,6 +32,8 @@ public class MathAlgorithms {
         if (x <= 0) {
             throw new IllegalArgumentException("Try with a positive number!");
         }
+        if (x == 1){return true;}
+
         int z = 0;
         boolean result = false;
         for (int y=1; y<x; y++) {
