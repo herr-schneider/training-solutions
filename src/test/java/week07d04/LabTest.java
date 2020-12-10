b345288b104c05e1645b548fd3ebb6015527f81d
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-12-10 15:47:05 +0100

    2nd

---
+++ src/test/java/week07d04/LabTest.java
@@ -0,0 +1,29 @@
+package week07d04;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LabTest {
+
+    @Test
+    public void testLab() {
+        Lab l = new Lab("Jeep");
+        assertEquals("Jeep", l.getTitle());
+        assertEquals(null, l.getCompletedAt());
+        assertEquals(false, l.isCompleted());
+    }
+
+    @Test
+    public void testLabComplete() {
+        Lab l = new Lab("Jeep");
+        l.completed();
+        assertEquals("Jeep", l.getTitle());
+        assertEquals(LocalDate.now(), l.getCompletedAt());
+        assertEquals(true, l.isCompleted());
+
+    }
+
+}
