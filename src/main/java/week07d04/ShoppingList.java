9250d86ba9acbb58ab5f07f44647515064950288
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-12-11 09:42:53 +0100

    1st

---
+++ src/main/java/week07d04/ShoppingList.java
@@ -0,0 +1,26 @@
+package week07d04;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.util.Scanner;
+
+public class ShoppingList {
+    static long sumOf;
+
+    public long calculateSum(String path) {
+        int result = 0;
+        try ( Scanner s = new Scanner(Path.of(path));){
+            s.useDelimiter(";");
+            while (s.hasNext()) {
+                String str = s.next();
+                int pieces = Integer.parseInt(s.next());
+                int price = Integer.parseInt(s.next());
+                result += pieces * price;
+            }
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("Wrong path!");
+        }
+        return result;
+    }
+}
+
---
+++ src/test/java/week07d04/ShoppingListTest.java
@@ -0,0 +1,25 @@
+package week07d04;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+import week07d01.MathAlgorithms;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class ShoppingListTest {
+
+    ShoppingList s = new ShoppingList();
+
+    @Test
+    public void testShopping() {
+
+        assertEquals(240, s.calculateSum("/training/training_solutions/src/main/java/week07d04/shoppingList.txt"));
+    }
+
+    public void testZeroInput() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> s.calculateSum(null));
+        Assertions.assertEquals("Wrong path!", ex.getMessage());
+    }
+}
