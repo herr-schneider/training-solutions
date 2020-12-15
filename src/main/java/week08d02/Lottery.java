e2bcded7349343653727c651f5850a9981b96529
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-12-15 18:11:43 +0100

    2st

--- src/main/java/week08d02/Lottery.java
+++ src/main/java/week08d02/Lottery.java
@@ -1,34 +1,27 @@
 package week08d02;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
 public class Lottery {
 
-    public List<Integer> getNumbers(int interval, int number, int s) {
-        List<Integer> winner = new ArrayList<>();
-        int before = 0;
-        int actual = 0;
-        for (int i = 1; i < number+1; i++) {
-            actual = new Random(s).nextInt(interval) + 1;
-            if (actual != before) {
-                winner.add(actual);
-            } else {
-                i = i - 1;
-            }
-            before = actual;
-        }
+    public List<Integer> getSortNumbers(List<Integer> winner) {
+        Collections.sort(winner);
         return winner;
     }
 
-    public List<Integer> sort(int number, int interval, int s){
+    public List<Integer> getNumbers(int number, int interval, int s) {
+        if (number >= interval){
+            throw new IllegalArgumentException("Invalid!");
+        }
         List<Integer> winner = new ArrayList<>();
-        int before = 0;
+        Random random = new Random(s);
         int actual = 0;
-        for (int i = 1; i < number+1; i++) { // while (winner.size() < 5)
-            actual = new Random(s).nextInt(interval) + 1;
-            if (winner.contains(actual)) {
+        for (int i = 1; i < number + 1; i++) { // while (winner.size() < 5)
+            actual = random.nextInt(interval) + 1;
+            if (!winner.contains(actual)) {
                 winner.add(actual);
             } else {
                 i = i - 1;
@@ -37,21 +30,18 @@ public class Lottery {
         return winner;
     }
 
-    public List<Integer> get5Numbers() {
+    public List<Integer> get5Numbers(int s) { // getNumbers(5, 90, 5);
         List<Integer> winner = new ArrayList<>();
-        int before = 0;
+        Random random = new Random(s);
         int actual = 0;
-        for (int i = 1; i < 6; i++) {
-            actual = new Random().nextInt(89) + 1;
-            if (actual != before) {
+        while (winner.size() < 5) {
+            actual = random.nextInt(89) + 1;
+            if (!winner.contains(actual)) {
                 winner.add(actual);
-            } else {
-                i = i - 1;
             }
-            before = actual;
         }
-        return winner;
-    }
+        return winner;    }
+
     public void printNumbers(List<Integer> winner) {
         for (int i : winner) {
             System.out.println(i);
--- src/test/java/week08d02/LotteryTest.java
+++ src/test/java/week08d02/LotteryTest.java
@@ -11,8 +11,18 @@ public class LotteryTest {
 
     @Test
     public void numbersTest(){
-        List<Integer> numbers = Arrays.asList(41,7,39,4,17,1);
+        List<Integer> numbers;
         Lottery l = new Lottery();
-        assertEquals(numbers,l.getNumbers(45, 6, 5));
+        numbers = Arrays.asList(73, 89, 54, 72, 19);
+        assertEquals(numbers,l.get5Numbers(5));
+        numbers = Arrays.asList(18,38,45,15,22,36);
+        assertEquals(numbers,l.getNumbers(6, 45, 5));
     }
+    @Test
+    public void sortingTest(){
+        Lottery l = new Lottery();
+        List<Integer> numbers = Arrays.asList(15,18,22,36,38,45);
+        assertEquals(numbers,l.getSortNumbers(l.getNumbers(6, 45, 5)));
+    }
+
 }
