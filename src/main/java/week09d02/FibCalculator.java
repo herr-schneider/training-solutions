852800894708187b38c3d25db7f3ba0f79bc65b6
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-12-22 11:18:14 +0100

    1st commit

---
+++ src/main/java/week09d02/FibCalculator.java
@@ -0,0 +1,27 @@
+package week09d02;
+
+public class FibCalculator {
+
+    public long sumEvents(int bount) {
+        int n = 2;
+        int nMinus = 1;
+        int temp = 0;
+        long result = 0;
+        while ((n + nMinus) < bount) {
+            if (n % 2 == 0) {
+                result += n;
+            }
+            temp = n + nMinus;
+            nMinus = n;
+            n = temp;
+        }
+        return result;
+    }
+
+    public static void main(String[] args) {
+        FibCalculator f = new FibCalculator();
+        System.out.println(f.sumEvents(20));
+    }
+}
+
+
