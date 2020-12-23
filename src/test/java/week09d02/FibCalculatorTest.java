9fe0cd906aa139d87bb4b807e5dc25efaa17ea31
szabo ferenc <szaboferenc.kromat@gmail.com>
2020-12-23 11:10:36 +0100

    modified

--- src/test/java/week09d02/FibCalculatorTest.java
+++ src/test/java/week09d02/FibCalculatorTest.java
@@ -9,6 +9,12 @@ public class FibCalculatorTest {
     @Test
     public void testFib() {
         FibCalculator f = new FibCalculator();
-       assertEquals(10,f.sumEvents(20));
+        assertEquals(10, f.sumEvents(20));
+    }
+
+    @Test
+    public void testFib40() {
+        FibCalculator f = new FibCalculator();
+        assertEquals(10, f.sumEvents(40));
     }
 }
