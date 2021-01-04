ee60fb96dd0929a5bdaacdfaf68224cbd51ee8cd
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-04 17:10:13 +0100

    ver 1.0

--- src/main/java/week10d01/Hiking.java
+++ src/main/java/week10d01/Hiking.java
@@ -5,18 +5,22 @@ import java.util.List;
 
 public class Hiking {
     private List<Double> altitude = new ArrayList<>();
+    private double coordinateLong;
+    private double coordinateVert;
 
     public Hiking(List<Double> altitude) {
         this.altitude = altitude;
     }
 
     public Hiking(double... altitude) {
+
         for (double alt : altitude) {
             this.altitude.add(alt);
         }
     }
 
     public double getPlusElevation(){
+        if (altitude == null || altitude.size() < 2){throw new IllegalArgumentException("Nem jó!");}
         double result = 0;
         for (int i = 1; i<altitude.size(); i++) {
          if ((altitude.get(i)-altitude.get(i-1))>0) {result += (altitude.get(i)-altitude.get(i-1));}
--- src/test/java/week10d01/HikingTest.java
+++ src/test/java/week10d01/HikingTest.java
@@ -1,14 +1,32 @@
 package week10d01;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class HikingTest {
 
     @Test
-    public void testHiking(){
+    public void testHiking() {
         Hiking h = new Hiking(10.0, 20.0, 15.0, 18.0);
         assertEquals(13, h.getPlusElevation());
+    }
+
+    @Test
+    public void testHikingList() {
+
+        assertEquals(13, new Hiking(Arrays.asList(10.0, 20.0, 15.0, 18.0)).getPlusElevation());
+    }
+
+    @Test
+    public void testHikingNull() {
+        Exception ex = assertThrows(IllegalArgumentException.class, () -> new Hiking(new ArrayList<>()).getPlusElevation());
+        Assertions.assertEquals("Nem jó!", ex.getMessage());
     }
 }
