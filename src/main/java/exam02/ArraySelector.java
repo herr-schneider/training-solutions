dc23d1bcc08886ff1e8403f9d76b2c0cf7516ce9
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-06 17:23:30 +0100

    1st commit

---
+++ src/main/java/exam02/ArraySelector.java
@@ -0,0 +1,14 @@
+package exam02;
+
+public class ArraySelector {
+
+    public String selectEvens (int[] numbers){
+        if (numbers.length<1){return "";}
+        StringBuilder result =new StringBuilder("[");
+        result.append(numbers[0]);
+        for (int i = 2; i < numbers.length; i += 2){
+result.append(", " + numbers[i]);
+        }
+        return result.append("]").toString();
+    }
+}
