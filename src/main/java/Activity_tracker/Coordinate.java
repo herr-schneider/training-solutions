20d09dab59c38686ff9fc7ad7f6df410c35f62a2
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-09 16:42:53 +0100

    first commit

--- src/main/java/Activity_tracker/Coordinate.java
+++ src/main/java/Activity_tracker/Coordinate.java
@@ -6,6 +6,7 @@ public class Coordinate {
 
 
     public Coordinate(double latitude, double longitude) {
+        if (latitude<-91 || latitude > 90 || longitude<-180 || longitude>180){throw new IllegalArgumentException();}
         this.latitude = latitude;
         this.longitude = longitude;
     }
