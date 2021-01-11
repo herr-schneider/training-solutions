da6607d57a62fc0d8d7b5ba976427e3f02722837
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-11 15:47:24 +0100

    1st commit, de nem vagyok büszke rá

--- src/main/java/week11d01/DivisorFinder.java
+++ src/main/java/week11d01/DivisorFinder.java
@@ -3,9 +3,22 @@ package week11d01;
 public class DivisorFinder {
 
     public int findDivisors(int n){
+        if (n>1000) {return findDivisors100(n);}
         if (n>100) {return findDivisors100(n);}
         if (n>10) {return findDivisors10(n); }
         return 0;
+    }
+    public int findDivisors1000(int n){
+        int result = 0;
+        int thousand = n/1000;
+        int hundred = (n-thousand*1000)/100;
+        int deca = (n-hundred*100)/10;
+        int single = n-(hundred*100+deca*10);
+        if (n%thousand == 0){result++;}
+        if (n%hundred == 0){result++;}
+        if (n%deca == 0){result++;}
+        if (n%single == 0){result++;}
+        return result;
     }
 
     public int findDivisors100(int n){
