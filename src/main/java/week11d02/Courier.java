48fef8925cf7348f68ce98ba3ef14158cfc285d4
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-15 18:48:22 +0100

    corrected

--- src/main/java/week11d02/Courier.java
+++ src/main/java/week11d02/Courier.java
@@ -38,7 +38,7 @@ public class Courier {
 
     }
 
-   public int firstFreeDay() {
+   public int firstFreeDay() { // müködik
         int result = -1;
         Ride beforeRide = new Ride();
         for (int i = 1; i<rides.size(); i++) {
@@ -46,40 +46,39 @@ public class Courier {
                result = rides.get(i-1).getDay()+1;
             }
         }
-        return result;
+        return rides.get(rides.size()-1).getDay() < 7 ? rides.get(rides.size()-1).getDay()+1 : result;
     }
 
     public List<Ride> getRides() {
         return rides;
     }
 
-    public void addRide3(Ride ride) {
+    public void addRide3(Ride ride) { // müködik
         if (ride == null) {
             throw new IllegalArgumentException("Ride is null");
         }
-        if (rides.size() == 0) {
+        int sizeOfList = rides.size();
+        if (sizeOfList == 0) {
             rides.add(ride);
-        }
-        if (rides.get(rides.size() - 1).getDay() <= ride.getDay() &&
-                rides.get(rides.size() - 1).getNumberOfRide() < ride.getNumberOfRide()) {
+        } else if (rides.get(sizeOfList - 1).getDay() < ride.getDay())  {
             rides.add(ride);
-        }
+        } else if (rides.get(sizeOfList - 1).getDay() == ride.getDay()
+                && rides.get(rides.size() - 1).getNumberOfRide() < ride.getNumberOfRide()){
+            rides.add(ride);
+        } else{
+            System.out.println("nem jó");}
 
     }
 
     public int freeDay() {
         int result = -1;
-
-        for (int i = 1; i < rides.get(rides.size()).getDay(); i++) {
-            for (Ride item : rides
-            ) {
+        for (int i = 1; i < rides.get(rides.size()-1).getDay(); i++) {
+            for (Ride item : rides) {
                 if (i != item.getDay()) {
                     result = i;
                 }
             }
         }
-
-
         return result;
     }
 }
--- src/test/java/week11d02/CourierTest.java
+++ src/test/java/week11d02/CourierTest.java
@@ -9,13 +9,37 @@ class CourierTest {
     @Test
     void addRide() {
         Courier c = new Courier();
-       c.addRide(new Ride(1,1,20));
-       c.addRide(new Ride(2,3,12));
-      // c.addRide(new Ride(2,2,12));
-
+        c.addRide(new Ride(1, 1, 20));
+        c.addRide(new Ride(2, 3, 12));
+        // c.addRide(new Ride(2,2,12));
     }
 
     @Test
-    void getRides() {
+    void addRide2() {
+        Courier c = new Courier();
+        c.addRide2(new Ride(1, 1, 20));
+        c.addRide2(new Ride(2, 3, 12));
+        // c.addRide(new Ride(2,2,12));
+    }
+
+    @Test
+    void addRide3() {
+        Courier c = new Courier();
+        c.addRide3(new Ride(1, 1, 20));
+        c.addRide3(new Ride(2, 3, 12));
+    }
+
+    @Test
+    void addRide3andFreeDay() {
+        Courier c = new Courier();
+        c.addRide3(new Ride(1, 1, 20));
+        c.addRide3(new Ride(2, 1, 12));
+        c.addRide3(new Ride(2, 2, 12));
+        c.addRide3(new Ride(3, 1, 20));
+        c.addRide3(new Ride(4, 2, 12));
+        c.addRide3(new Ride(4, 3, 12));
+        System.out.println(c.getRides().size());
+        System.out.println(c.firstFreeDay());
+        // System.out.println(c.freeDay());
     }
 }
