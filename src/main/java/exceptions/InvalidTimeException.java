28b271804501000819a576373a756a6ad2cf8651
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-16 14:36:08 +0100

    2nd commit

--- src/main/java/exceptions/InvalidTimeException.java
+++ src/main/java/exceptions/InvalidTimeException.java
@@ -5,4 +5,8 @@ public class InvalidTimeException extends RuntimeException{
     public InvalidTimeException(String massage){
         super(massage);
     }
+
+    public InvalidTimeException(String massage, Exception e){
+        super(massage, e);
+    }
 }
--- src/main/java/exceptions/SimpleTime.java
+++ src/main/java/exceptions/SimpleTime.java
@@ -2,21 +2,45 @@ package exceptions;
 
 public class SimpleTime {
     //Lehet létrehozni óra és perc megadásával és lehet időpontot megadni "hh:mm" alakú String-el is.
-    //Legyen felüldefiniálva a toString úgy, hogy "hh:mm" alakú időt adjon.
-    //Legyen getHour, és getMinute metódusa is.
+
 
     private int hour;
     private int minute;
 
     public SimpleTime(int hour, int minute) {
-        this.hour = hour;
-        this.minute = minute;
+        if (isCorrect(hour, minute)) {
+            this.hour = hour;
+            this.minute = minute;
+        } else {
+            throw new IllegalArgumentException("Unknown error!");
+        }
+    }
+
+    public boolean isCorrect(int hour, int minute) {
+        if (hour < 0 || hour > 23) {
+            throw new InvalidTimeException("Hour is invalid (0-23)");
+        }
+        if (minute < 0 || minute > 59) {
+            throw new InvalidTimeException("Minute is invalid (0-59)");
+        }
+        return true;
     }
 
     public SimpleTime(String hoursMinutes) {
+        if (hoursMinutes == null) {
+            throw new InvalidTimeException("Time is null");
+        }
         String[] hoursMinutesArray = hoursMinutes.split(":");
-        this.hour = Integer.parseInt(hoursMinutesArray[0]);
-        this.minute = Integer.parseInt(hoursMinutesArray[1]);
+        try {
+            int hour = Integer.parseInt(hoursMinutesArray[0]);
+            int minute = Integer.parseInt(hoursMinutesArray[1]);
+            if (isCorrect(hour, minute)) {
+                this.hour = hour;
+                this.minute = minute;
+            }
+        } catch (NumberFormatException nfe) {
+            throw new InvalidTimeException("Time is not hh:mm");
+        }
     }
 
     public int getHour() {
@@ -25,5 +49,10 @@ public class SimpleTime {
 
     public int getMinute() {
         return minute;
+    }
+
+    @Override
+    public String toString() {
+        return hour < 10 ? "0" + hour + ":" + minute : hour + ":" + minute;
     }
 }
