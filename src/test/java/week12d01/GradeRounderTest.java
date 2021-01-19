3318afaf63f0dc75f298bc0ee5bacf81dadae4e8
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-19 20:04:48 +0100

    1st commit

--- src/test/java/week12d01/GradeRounderTest.java
+++ src/test/java/week12d01/GradeRounderTest.java
@@ -2,8 +2,6 @@ package week12d01;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
-
 import static org.junit.jupiter.api.Assertions.*;
 
 class GradeRounderTest {
@@ -11,8 +9,6 @@ class GradeRounderTest {
     @Test
     public void roundGrades() {
         GradeRounder gr = new GradeRounder();
-
-
         assertEquals("[10, 20, 30, 50, 65, 70]", gr.toString((gr.roundGrades(new int[]{10, 20, 30, 50, 63, 72}))));
     }
 }
