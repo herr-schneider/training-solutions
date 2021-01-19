0ac200c43be9459ba98d3642a80f6d0588af61f0
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-19 16:33:49 +0100

    1st commit

---
+++ src/main/java/week12d02/FenceCondition.java
@@ -0,0 +1,34 @@
+package week12d02;
+
+public class FenceCondition {
+
+    private Fence fence;
+    private int numberOfFence;
+
+
+    public FenceCondition(Fence fence, int numberOfFence) {
+        this.fence = fence;
+        this.numberOfFence = numberOfFence;
+    }
+
+    public Fence getFence() {
+        return fence;
+    }
+
+    public int getNumberOfFence() {
+        return numberOfFence;
+    }
+
+    public void setFence(Fence fence) {
+        this.fence = fence;
+    }
+
+    public void setNumberOfFence(int numberOfFence) {
+        this.numberOfFence = numberOfFence;
+    }
+
+    @Override
+    public String toString() {
+        return fence + ": " + numberOfFence + " db";
+    }
+}
---
+++ src/test/java/week12d02/StreetTest.java
@@ -0,0 +1,48 @@
+package week12d02;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class StreetTest {
+
+    @Test
+    public void lastSoldSite() {
+        Street s = new Street();
+        s.sellSite(new Site(0, 10, Fence.PERFECT));
+        s.sellSite(new Site(0, 7, Fence.NEED_UPGRADE));
+        s.sellSite(new Site(1, 12, Fence.NEED_UPGRADE));
+        s.sellSite(new Site(0, 9, Fence.PERFECT));
+        s.sellSite(new Site(0, 11, Fence.NO_FENCE));
+        s.sellSite(new Site(1, 13, Fence.PERFECT));
+        assertEquals(3, s.lastSoldSite());
+    }
+
+    @Test
+    public void lastPlusOneSoldSite() {
+        Street s = new Street();
+        s.sellSite(new Site(0, 10, Fence.PERFECT));
+        s.sellSite(new Site(0, 7, Fence.NEED_UPGRADE));
+        s.sellSite(new Site(1, 12, Fence.NEED_UPGRADE));
+        s.sellSite(new Site(0, 9, Fence.PERFECT));
+        s.sellSite(new Site(0, 11, Fence.NO_FENCE));
+        s.sellSite(new Site(1, 13, Fence.PERFECT));
+        s.sellSite(new Site(0, 15, Fence.PERFECT));
+        assertEquals(10, s.lastSoldSite());
+        System.out.println(s.statOfFences().toString());
+    }
+
+    @Test
+    public void StatOfSoldSite() {
+        Street s = new Street();
+        s.sellSite(new Site(0, 10, Fence.PERFECT));
+        s.sellSite(new Site(0, 7, Fence.NEED_UPGRADE));
+        s.sellSite(new Site(1, 12, Fence.NEED_UPGRADE));
+        s.sellSite(new Site(0, 9, Fence.PERFECT));
+        s.sellSite(new Site(0, 11, Fence.NO_FENCE));
+        s.sellSite(new Site(1, 13, Fence.PERFECT));
+        s.sellSite(new Site(0, 15, Fence.PERFECT));
+        assertEquals(3, s.statOfFences().size());
+        assertEquals("[PERFECT: 4 db, NEED_UPGRADE: 2 db, NO_FENCE: 1 db]", s.statOfFences().toString());
+    }
+}
