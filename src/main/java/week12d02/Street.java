f388986a2f27a3ad23efb42d6340ecb16f4cfb34
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-19 17:03:33 +0100

    2st commit

--- src/main/java/week12d02/Street.java
+++ src/main/java/week12d02/Street.java
@@ -25,22 +25,42 @@ public class Street {
 
     public List<FenceCondition> statOfFences() {
         List<FenceCondition> result = new ArrayList<>();
-        int counter = 0;
         for (Fence fence : Fence.values()) {
+            int counter = 0;
             for (Site site : soldSites) {
                 if (site.getFence() == fence) {
                     counter++;
                 }
             }
             result.add(new FenceCondition(fence, counter));
-            counter = 0;
         }
+        return new ArrayList<>(result);
+    }
+
+    public List<FenceCondition> statOfFences2() { // nem jo, ha valtozik az enum, nem lesz automatikus
+        List<FenceCondition> result = new ArrayList<>();
+        int counterPerfect = 0;
+        int counterUpdate = 0;
+        int counterNoFence = 0;
+        for (Site site : soldSites) {
+            if (site.getFence() == Fence.PERFECT) {
+                counterPerfect++;
+            }
+            if (site.getFence() == Fence.NEED_UPGRADE) {
+                counterUpdate++;
+            }
+            if (site.getFence() == Fence.NO_FENCE) {
+                counterNoFence++;
+            }
+        }
+        result.add(new FenceCondition(Fence.PERFECT, counterPerfect));
+        result.add(new FenceCondition(Fence.NEED_UPGRADE, counterUpdate));
+        result.add(new FenceCondition(Fence.NO_FENCE, counterNoFence));
         return result;
     }
 
-
-        public List<Site> getSoldSite () {
-            return new ArrayList<>(soldSites);
-        }
-
+    public List<Site> getSoldSite() {
+        return new ArrayList<>(soldSites);
     }
+
+}
--- src/test/java/week12d02/StreetTest.java
+++ src/test/java/week12d02/StreetTest.java
@@ -45,4 +45,18 @@ class StreetTest {
         assertEquals(3, s.statOfFences().size());
         assertEquals("[PERFECT: 4 db, NEED_UPGRADE: 2 db, NO_FENCE: 1 db]", s.statOfFences().toString());
     }
+
+    @Test
+    public void StatOfSoldSite2() {
+        Street s = new Street();
+        s.sellSite(new Site(0, 10, Fence.PERFECT));
+        s.sellSite(new Site(0, 7, Fence.NEED_UPGRADE));
+        s.sellSite(new Site(1, 12, Fence.NEED_UPGRADE));
+        s.sellSite(new Site(0, 9, Fence.PERFECT));
+        s.sellSite(new Site(0, 11, Fence.NO_FENCE));
+        s.sellSite(new Site(1, 13, Fence.PERFECT));
+        s.sellSite(new Site(0, 15, Fence.PERFECT));
+        assertEquals(3, s.statOfFences2().size());
+        assertEquals("[PERFECT: 4 db, NEED_UPGRADE: 2 db, NO_FENCE: 1 db]", s.statOfFences2().toString());
+    }
 }
