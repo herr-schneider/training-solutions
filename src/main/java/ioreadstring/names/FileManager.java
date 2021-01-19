82c157a7546eb0894097bbb3c6b8315e63b3255b
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-01-19 17:05:13 +0100

    3rd commit

--- src/main/java/ioreadstring/names/FileManager.java
+++ src/main/java/ioreadstring/names/FileManager.java
@@ -21,12 +21,7 @@ public class FileManager {
         separator = FileSystems.getDefault().getSeparator();
     }
 
-    // A feladat egy szöveges állományból nevek beolvasása és eltárolása egy listába.
-    // A megoldáshoz két osztály kell megvalósítanod. A Human osztály reprezentál egy embert.
-    // Két adattagja vezeték- illetve keresztnév. A FileManager osztály felelős a fájl feldolgozásért.
-    // Egy Path típusú attribútumon keresztül érjük el a fájlt,
-    // míg a readFromFile() metódus felelős a beolvasásért és a Human objektumok létrehozásáért.
-
+    // Path file = Path.of("employees").resolve("a.dat");
 
     public FileManager(String pof) {
         this.pathOfFile = Path.of(pof);
