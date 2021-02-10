a3e368f31aa874cda3dc53e7696fc89fdea2cc68
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-02-10 16:28:47 +0100

    2nd commit

--- src/main/java/week15d03/PostFinder.java
+++ src/main/java/week15d03/PostFinder.java
@@ -3,12 +3,16 @@ package week15d03;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class PostFinder {
 
     private List<Post> posts = new ArrayList<>();
 
     public PostFinder(List<Post> posts) {
+        if (posts.isEmpty()) {
+            throw new IllegalArgumentException("Empty list!");
+        }
         this.posts = posts;
     }
 
@@ -17,8 +21,12 @@ public class PostFinder {
     }
 
     public List<Post> findPostsFor(String user) {
+        Objects.requireNonNull(user, "Hulye vagy!"); // hibat dob, ha a user null
         List<Post> result = new ArrayList<>();
         for (Post post : posts) {
+            if (post.getPublishedAt() == null){
+                throw new IllegalArgumentException("\"Publish at\" is NULL");
+            }
             if (post.getOwner().equals(user) &&
                     !post.getContent().isEmpty() &&
                     !post.getTitle().isEmpty() &&
--- src/test/java/week15d03/PostFinderTest.java
+++ src/test/java/week15d03/PostFinderTest.java
@@ -1,5 +1,6 @@
 package week15d03;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.time.LocalDate;
@@ -10,16 +11,27 @@ import static org.junit.jupiter.api.Assertions.*;
 public class PostFinderTest {
 
     @Test
-    public void testPost(){
-        Post postman = new Post("postman st 12.", LocalDate.of(2020,12,30),"com", "Feri");
-        Post jozsi = new Post("valami u. 14.", LocalDate.of(2020,12,29),"com", "Jozsi");
-        Post pisti = new Post("hgt u. 12.", LocalDate.of(2020,6,30),"com", "Pisti");
-        Post dani = new Post("pohos u. 32", LocalDate.of(2020,8,30),"com", "Dani");
-        Post vlk = new Post("postman", LocalDate.of(2020,11,30),"com", "Feri");
-        PostFinder pf = new PostFinder(List.of(postman,jozsi,pisti,dani,vlk));
+    public void testPost() {
+        Post postman = new Post("postman st 12.", LocalDate.of(2020, 12, 30), "com", "Feri");
+        Post jozsi = new Post("valami u. 14.", LocalDate.of(2020, 12, 29), "com", "Jozsi");
+        Post pisti = new Post("hgt u. 12.", LocalDate.of(2020, 6, 30), "com", "Pisti");
+        Post dani = new Post("pohos u. 32", LocalDate.of(2020, 8, 30), "com", "Dani");
+        Post vlk = new Post("postman", LocalDate.of(2020, 11, 30), "com", "Feri");
+        PostFinder pf = new PostFinder(List.of(postman, jozsi, pisti, dani, vlk));
 
         assertEquals("[Post{title='postman st 12.', publishedAt=2020-12-30, content='com', owner='Feri'}, Post{title='postman', publishedAt=2020-11-30, content='com', owner='Feri'}]",
                 pf.findPostsFor("Feri").toString());
     }
 
+    @Test
+    public void testException() {
+        Post postman = new Post("postman st 12.", LocalDate.of(2020, 12, 30), "com", "Feri");
+        Post jozsi = new Post("valami u. 14.", LocalDate.of(2020, 12, 29), "com", "Jozsi");
+        Post pisti = new Post("hgt u. 12.", LocalDate.of(2020, 6, 30), "com", "Pisti");
+        Post dani = new Post("pohos u. 32", LocalDate.of(2020, 8, 30), "com", "Dani");
+        Post vlk = new Post("postman", LocalDate.of(2020, 11, 30), "com", "Feri");
+        PostFinder pf = new PostFinder(List.of(postman, jozsi, pisti, dani, vlk));
+        Exception ex = assertThrows(NullPointerException.class, () -> pf.findPostsFor(null));
+        Assertions.assertEquals("Hulye vagy!", ex.getMessage());
+    }
 }
