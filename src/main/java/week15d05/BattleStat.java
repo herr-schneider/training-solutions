5eaf5b824a752e0f8a0079c83c157b706923f39a
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-02-12 17:07:59 +0100

    3rd commit

--- src/main/java/week15d05/BattleStat.java
+++ src/main/java/week15d05/BattleStat.java
@@ -1,11 +1,14 @@
 package week15d05;
 
+import collectionsiterator.Book;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.Charset;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
+
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -15,33 +18,53 @@ public class BattleStat {
     private final Charset utf8 = StandardCharsets.UTF_8;
     private final Charset win = Charset.forName("Windows-1250");
 
-    private final int topCases = 3;
+    private int maxBattle = 0;
 
     public Map<String, Integer> readFile() {
         Path file = Path.of("src/main/java/week15d05/battles.csv");
         Map<String, Integer> cases = new TreeMap<>();
+
+        int actual = 0;
         try (BufferedReader bf = Files.newBufferedReader(file, utf8)) {
             String line;
             bf.readLine();
             while ((line = bf.readLine()) != null) {
                 String[] temp = line.split(",");
-                for (int i = 5; i < 13; i++) {
+                for (int i = 5; i < 13; i++) { // Set kivedheto lenne a defender és attacker ugyanaz
+                    if (temp[i] == null || temp[i].isEmpty()) {
+                        continue;
+                    }
                     if (!cases.containsKey(temp[i])) {
                         cases.put(temp[i], 1);
                     } else {
-                        cases.put(temp[i], cases.get(temp[i]) + 1);
+                        actual = cases.get(temp[i]);
+                        cases.replace(temp[i], actual + 1);
+                        if (actual > maxBattle) {
+                            maxBattle = actual+1;
+                        }
                     }
                 }
             }
         } catch (IOException ioe) {
             throw new IllegalArgumentException("File not found", ioe);
         }
-
         return cases;
     }
+
+    public Map.Entry<String, Integer> maxBattles(Map<String, Integer> battles) {
+        for (Map.Entry<String, Integer> battle : battles.entrySet()) {
+            if (battle.getValue() == maxBattle) {
+                return battle;
+            }
+        }
+        throw new IllegalStateException("No maximum");
+    }
+
 
     public static void main(String[] args) {
         BattleStat bt = new BattleStat();
         System.out.println(bt.readFile());
+        System.out.println(bt.maxBattle);
+        System.out.println(bt.maxBattles(bt.readFile()));
     }
 }
