8b9a3888c2d7d56794847885b2b4fbef52992b8a
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-02-13 13:04:45 +0100

    ver 2.0

--- src/main/java/week15d05/BattleStat.java
+++ src/main/java/week15d05/BattleStat.java
@@ -9,6 +9,8 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
 
@@ -23,13 +25,13 @@ public class BattleStat {
     public Map<String, Integer> readFile() {
         Path file = Path.of("src/main/java/week15d05/battles.csv");
         Map<String, Integer> cases = new TreeMap<>();
-
         int actual = 0;
         try (BufferedReader bf = Files.newBufferedReader(file, utf8)) {
             String line;
             bf.readLine();
             while ((line = bf.readLine()) != null) {
-                String[] temp = line.split(",");
+
+                String[] temp = process(line);
                 for (int i = 5; i < 13; i++) { // Set kivedheto lenne a defender és attacker ugyanaz
                     if (temp[i] == null || temp[i].isEmpty()) {
                         continue;
@@ -40,7 +42,7 @@ public class BattleStat {
                         actual = cases.get(temp[i]);
                         cases.replace(temp[i], actual + 1);
                         if (actual > maxBattle) {
-                            maxBattle = actual+1;
+                            maxBattle = actual + 1;
                         }
                     }
                 }
@@ -49,6 +51,23 @@ public class BattleStat {
             throw new IllegalArgumentException("File not found", ioe);
         }
         return cases;
+    }
+
+    public String[] process(String line) {
+        List<String> temp = new ArrayList<>();
+        String[] result;
+        if (!line.contains("\"")) {
+            result = line.split(",");
+        } else {
+            String[] modLine = line.split("\"");
+            for (int i = 0; i < modLine.length; i += 2) {
+                for (String row : modLine[i].split(",")) {
+                    temp.add(row);
+                }
+            }
+            result = temp.toArray(new String[0]);
+        }
+        return result;
     }
 
     public Map.Entry<String, Integer> maxBattles(Map<String, Integer> battles) {
@@ -67,4 +86,31 @@ public class BattleStat {
         System.out.println(bt.maxBattle);
         System.out.println(bt.maxBattles(bt.readFile()));
     }
+
+//    public String[] add(String[] first, String[] second) {
+//        int length = first.length < second.length ? first.length : second.length;
+//        String[] result = new String[length];
+//        for (int i = 0; i < length; i++) {
+//            result[i] = first[i] + second[i];
+//        }
+//        return result;
+//    }
+//
+//    public  String[] process(String line) {
+//        String[] result;
+//        if (!line.contains("\"")) {
+//            result = line.split(",");
+//        } else {
+//            String[] modLine = line.split("\"");
+//            result = modLine[0].split(",");
+//            int i = 2;
+//            while(i < modLine.length-1) {
+//                String[] temp2 = modLine[i].split(",");
+//                result = add(result, temp2);
+//                i += 2;
+//            }
+//        }
+//        return result;
+//    }
+
 }
