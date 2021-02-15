c066ddda2cefb38a3d56d7843ad0d74fd0ed0efa
szabo ferenc <szaboferenc.kromat@gmail.com>
2021-02-15 18:56:55 +0100

    Exam03

--- src/main/java/exam03/Histogram.java
+++ src/main/java/exam03/Histogram.java
@@ -1,6 +1,24 @@
 package exam03;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 
 public class Histogram {
+    public String createHistogram(BufferedReader reader) {
+        StringBuilder results = new StringBuilder();
+        try {
+            String line;
+            while ((line = reader.readLine()) != null) {
+                int n = Integer.parseInt(line);
+                for (int i = 0; i < n; i++) {
+                   results.append("*");
+                }
+                results.append("\n");
+            }
+        } catch (IOException ioe) {
+            throw new IllegalArgumentException("File not found", ioe);
+        }
+        return results.toString();
+    }
+}
 
